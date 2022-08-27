5f696845b822cc6d04225183918f9e168eb01d80
jay1942la <112227851+jay1942la@users.noreply.github.com>
2022-08-27 12:17:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,13 +5,13 @@ export const config = {
     // 公众号appSecret
     appSecret: "3ae4ec97247ec62211b2dce748247f95",
     // 模板消息id
-    templateId: "uI_i9Gggu-wryNrEZTsJfLxU9pHrCqyEo0urrLp69oY",
+    templateId: "pyAfRMQ9BK-brKOBCHSFaOpLweiCB_58j7uzLAZn4gc",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: ["o_7CK5jRG1cbjY4hEP_Ok_yen3ok"],
      
     // 信息配置
     // 所在省份
-    province: "江苏",
+    province: "江苏省",
     // 所在城市
     city: "南通",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
@@ -29,7 +29,7 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
+
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
